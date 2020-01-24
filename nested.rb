03d1f9651263e8ba0b87cd62894cba3fd9a0eee3
Asad Jafri <asadhjafri@gmail.com>
2020-01-24 04:56:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -68,20 +68,20 @@ def adding_matz
 # }
 
 	programmer_hash =
- 		{
-				:grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
+	{
+			:grace_hopper => {
+				:known_for => "COBOL",
+				:languages => ["COBOL", "FORTRAN"]
+			},
+			:alan_kay => {
+				:known_for => "Object Orientation",
+				:languages => ["Smalltalk", "LISP"]
+			},
+			:dennis_ritchie => {
+				:known_for => "Unix",
+				:languages => "C"
+			}
+	 }
 
 programmer_hash[0]=
 end
